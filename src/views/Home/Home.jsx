365f2ca24442352d5878392393933941109f6010
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-12 15:46:42 -0300

    home update

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
 
 import CardUser from '../../components/CardUser/CardUser'
+import CardTask from '../../components/CardTask/CardTask'
 
-import { Container } from './HomeStyle'
+import { Container, Content, Header, HeaderTitle } from './HomeStyle'
 
 const Home = () => {
   return (
     <>
       <StatusBar />
       <Container>
-        <CardUser />
+        <Content>
+          <CardUser />
+          <Header>
+            <HeaderTitle>Current Task</HeaderTitle>
+          </Header>
+          <CardTask />
+          <Header>
+            <HeaderTitle>Upcoming Task</HeaderTitle>
+          </Header>
+          <CardTask />
+        </Content>
       </Container>
     </>
   )
--- src/views/Home/HomeStyle.js
+++ src/views/Home/HomeStyle.js
@@ -5,3 +5,20 @@ export const Container = styled.SafeAreaView`
   background-color: #fff;
   align-items: center;
 `
+export const Content = styled.ScrollView`
+  flex: 1;
+  width: 100%;
+  padding-left: 25px;
+`
+
+export const Header = styled.View`
+  width: 100 %;
+  align-items: flex-start;
+`
+export const HeaderTitle = styled.Text`
+  fontsize: 20px;
+  font-weight: bold;
+  color: #050c31;
+  margin-left: 40px;
+  margin-bottom: 20px;
+`
