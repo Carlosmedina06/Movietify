f095b9b57946303778e5076dc6880ad3c2fd05ca
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-12 13:13:40 -0300

    configure redux toolkit

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import taskReducer from '../features/Task/TaskSlice'
+
+export const store = configureStore({
+  reducer: {
+    task: taskReducer,
+  },
+})
