5f38f80220ac7615a6952d0792b9fa2c4a32cf6d
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-18 01:48:37 -0300

    home update

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,21 +1,79 @@
+import { useState } from 'react'
+import { ScrollView } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
 import CardUser from '../../components/UserCard/UserCard'
 import TaskList from '../../components/TaskList/TaskList'
 import { data } from '../../../data'
 
-import { HomeContainer, HomeTitle } from './HomeStyle'
+import {
+  FloatingButton,
+  FloatingButtonText,
+  HideComplete,
+  HideCompleteText,
+  HomeContainer,
+  HomeHeader,
+  HomeTitle,
+} from './HomeStyle'
 
 export default function Home() {
+  const navigation = useNavigation()
+  const [hideCompleted, setHideCompleted] = useState(false)
+  const [localData, setLocalData] = useState(
+    data.sort((a, b) => {
+      return a.completed - b.completed
+    }),
+  )
+
+  const handleHideCompleted = () => {
+    if (hideCompleted) {
+      setHideCompleted(false)
+      setLocalData(
+        data.sort((a, b) => {
+          a.completed - b.completed
+        }),
+      )
+
+      return
+    }
+    setHideCompleted(!hideCompleted)
+    setLocalData(
+      localData.filter((task) => {
+        return !task.completed
+      }),
+    )
+  }
+
   return (
-    <HomeContainer>
-      <CardUser />
-      <HomeTitle>Today Task</HomeTitle>
-      <TaskList Tasks={data.filter((task) => task.isToday)} />
-      <HomeTitle>Upcomming Tasks</HomeTitle>
-      <TaskList
-        Tasks={data.filter((task) => {
-          return !task.isToday
-        })}
-      />
-    </HomeContainer>
+    <>
+      <HomeContainer>
+        <ScrollView>
+          <CardUser />
+          <HomeHeader>
+            <HomeTitle>Today Task</HomeTitle>
+            <HideComplete onPress={handleHideCompleted}>
+              <HideCompleteText>
+                {hideCompleted ? 'Show completed' : 'Hide completed'}
+              </HideCompleteText>
+            </HideComplete>
+          </HomeHeader>
+          <TaskList Tasks={localData.filter((task) => task.isToday)} />
+          <HomeHeader>
+            <HomeTitle>Upcoming Task</HomeTitle>
+            <HideComplete>
+              <HideCompleteText>See More →</HideCompleteText>
+            </HideComplete>
+          </HomeHeader>
+          <TaskList
+            Tasks={localData.filter((task) => {
+              return !task.isToday
+            })}
+          />
+        </ScrollView>
+      </HomeContainer>
+      <FloatingButton onPress={() => navigation.navigate('AddTask')}>
+        <FloatingButtonText>+</FloatingButtonText>
+      </FloatingButton>
+    </>
   )
 }
--- src/views/Home/HomeStyle.js
+++ src/views/Home/HomeStyle.js
@@ -1,13 +1,49 @@
 import styled from 'styled-components/native'
 
-export const HomeContainer = styled.View`
+export const HomeContainer = styled.SafeAreaView`
   flex: 1;
   background-color: #fff;
+`
+
+export const HomeHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 `
 
 export const HomeTitle = styled.Text`
   font-size: 24px;
   color: #050c31;
   font-weight: bold;
-  margin: 20px;
+`
+export const HideComplete = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+`
+export const HideCompleteText = styled.Text`
+  font-size: 14px;
+  color: #050c31;
+  font-weight: bold;
+`
+export const FloatingButton = styled.TouchableOpacity`
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  background-color: #050c31;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 5px;
+  position: absolute;
+  bottom: 50px;
+  right: 20px;
+  shadow-color: #050c31;
+  shadow-offset: 0px 0px;
+  shadow-opacity: 0.5;
+  shadow-radius: 5px;
+  elevation: 5;
+`
+export const FloatingButtonText = styled.Text`
+  font-size: 40px;
+  color: #fff;
+  font-weight: bold;
 `
