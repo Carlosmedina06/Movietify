6dab276a73b5749e019e6966987129769af659c2
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-18 01:48:48 -0300

    add navigation

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { SafeAreaView, ScrollView } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Home from './src/views/Home/Home'
+import AddTask from './src/views/AddTask/AddTask'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <Home />
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={Home} name="Home" />
+        <Stack.Screen
+          component={AddTask}
+          name="AddTask"
+          options={{
+            presentation: 'modal',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
