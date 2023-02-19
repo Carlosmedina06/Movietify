7d363f305d578930a265db7a637c962e8a50d1d5
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-19 02:45:36 -0300

    Provider

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Provider } from 'react-redux'
 
 import Home from './src/views/Home/Home'
 import AddTask from './src/views/AddTask/AddTask'
+import { store } from './src/redux/store'
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen component={Home} name="Home" />
-        <Stack.Screen
-          component={AddTask}
-          name="AddTask"
-          options={{
-            presentation: 'modal',
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen component={Home} name="Home" />
+          <Stack.Screen
+            component={AddTask}
+            name="AddTask"
+            options={{
+              presentation: 'modal',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   )
 }
