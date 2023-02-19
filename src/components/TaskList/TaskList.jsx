79b0b0e9885f9e7bb046322dfe6f83a8187a9003
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-19 02:45:55 -0300

    send id by props

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,25 +1,24 @@
-import { View, FlatList } from 'react-native'
+import { FlatList } from 'react-native'
 import React from 'react'
 
 import TaskCard from '../TaskCard/TaskCard'
 
 const TaskList = ({ Tasks }) => {
   return (
-    <View>
-      <FlatList
-        data={Tasks}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <TaskCard
-            completed={item.completed}
-            date={item.date}
-            hour={item.hour}
-            isToday={item.isToday}
-            title={item.title}
-          />
-        )}
-      />
-    </View>
+    <FlatList
+      data={Tasks}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TaskCard
+          completed={item.completed}
+          date={item.date}
+          hour={item.hour}
+          id={item.id}
+          isToday={item.isToday}
+          title={item.title}
+        />
+      )}
+    />
   )
 }
 
