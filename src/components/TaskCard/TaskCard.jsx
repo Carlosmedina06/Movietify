f7db7932140c9d7ed4b7b2681aa603e78781c16c
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-19 02:45:27 -0300

    function delete and complete task

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,3 +1,8 @@
+import { useDispatch, useSelector } from 'react-redux'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { updateTask, deleteTask } from '../../redux/taskSlice.js'
+
 import {
   TaskCardBody,
   TaskCardButton,
@@ -10,7 +15,51 @@ import {
   TaskContainer,
 } from './TaskCardStyle.js'
 
-const TaskCard = ({ _id, title, completed, hour, isToday, date }) => {
+const TaskCard = ({ id, title, completed, hour, isToday, date }) => {
+  const dispatch = useDispatch()
+  const tasks = useSelector((state) => state.tasks.tasks)
+
+  const handleCompleteTask = async () => {
+    try {
+      dispatch(updateTask({ id, completed }))
+      await AsyncStorage.setItem(
+        '@tasks',
+        JSON.stringify(
+          tasks.map((task) => {
+            if (task.id === id) {
+              return {
+                ...task,
+                completed: !completed,
+              }
+            }
+
+            return task
+          }),
+        ),
+      )
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error)
+    }
+  }
+
+  const handleDeleteTask = async () => {
+    try {
+      dispatch(deleteTask(id))
+      await AsyncStorage.setItem(
+        '@tasks',
+        JSON.stringify(
+          tasks.filter((task) => {
+            return task.id !== id
+          }),
+        ),
+      )
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error)
+    }
+  }
+
   return (
     <TaskContainer>
       <TaskCardHeader>
@@ -35,10 +84,10 @@ const TaskCard = ({ _id, title, completed, hour, isToday, date }) => {
         </TaskCardItem>
       </TaskCardBody>
       <TaskCardFooter>
-        <TaskCardButton>
+        <TaskCardButton onPress={handleCompleteTask}>
           <TaskCardButtonText primary>{completed ? 'Undone ❌' : 'Done ✅'}</TaskCardButtonText>
         </TaskCardButton>
-        <TaskCardButton primary>
+        <TaskCardButton primary onPress={handleDeleteTask}>
           <TaskCardButtonText>Delete 🗑️</TaskCardButtonText>
         </TaskCardButton>
       </TaskCardFooter>
